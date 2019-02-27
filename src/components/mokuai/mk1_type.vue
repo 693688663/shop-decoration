1e2232fa88184560a11b841a42286094a6930dd5
yxy982694 <37136741+yxy982694@users.noreply.github.com>
2019-02-27 10:18:51 +0800

    回显错误问题

--- src/components/mokuai/mk1_type.vue
+++ src/components/mokuai/mk1_type.vue
@@ -7,7 +7,7 @@
          <li class="fl" :class="{active:li_active==2}" @click="li_active=2">电脑端显示设置</li>
       </ul>
       <div class="con">
-         <div class="Baby_set" v-if="li_active==1">
+         <div class="Baby_set" v-show="li_active==1">
             <div class="li">
                <span class="type">推荐方式：</span>
                <span class="messagea">
@@ -70,7 +70,33 @@
                </span>
             </div>
          </div>
-         <p class="operation"><span class="save" @click="save_fun()">保存</span><span @click="edit_mk_data_fun(false)">取消</span></p>
+         <!-- 显示设置 -->
+         <div class="display_set" v-if="li_active==2">
+            <div class="li">
+               <span class="type">显示设置：</span>
+               <span class="messagea">
+                  <label><input name="display_set" type="radio" value="0" />不显示</label>
+                  <label><input name="display_set" type="radio" value="1" checked="checked" />显示</label>
+               </span>
+               <!-- <input type="text" v-model=""> -->
+            </div>
+            <div class="li">
+               <span class="type">展示方式：</span>
+               <span class="message">
+
+               </span>
+            </div>
+            <div class="li">
+               <span class="type">是否显示：</span>
+               <span class="message">
+
+               </span>
+            </div>
+         </div>
+         <p class="operation">
+            <span class="save" @click="save_fun()">保存</span>
+            <span @click="edit_mk_data_fun(false)">取消</span>
+         </p>
       </div>
    </div>
 </template>
@@ -83,6 +109,7 @@
             li_active: 1,//显示切换
             //基本设置信息
             data: {
+               name: "",// 模块名
                recommend_ways: "1",// 推荐方式
                recommend_sort_type: "1",//自动推荐排序
                baby_classify: "1",//宝贝分类
@@ -100,36 +127,14 @@
       }),
       mounted: function () {
          var that = this
-         var site = that.edit_mk_data
-         var data_list;
-         if (site.location1 == "hd") {
-            data_list = that.layout_data.hd[site.location4].data
-         }
-         if (site.location1 == "ft") {
-            data_list = that.layout_data.ft[site.location4].data
-         }
-         if (site.location1 == "con") {
-            if (site.location3 == "w19") {
-               data_list = that.layout_data.con[site.location2].w19[site.location4].data
-            }
-            if (site.location3 == "w75") {
-               data_list = that.layout_data.con[site.location2].w75[site.location4].data
-            }
-            if (site.location3 == "center") {
-               data_list = that.layout_data.con[site.location2].w1920[site.location4].data
-            }
-         }
-         if (data_list.baby_type) {
-            that.data = data_list
-         }
-
-
+         that.get_data()
       },
       methods: {
-         // 隐藏编辑模块
+         // 隐藏模块编辑弹窗
          edit_mk_data_fun(val) {
             var that = this
             var dispatch = this.$store.dispatch
+            // 隐藏模块
             dispatch({
                type: "mokuai_mask_ac",
                data: val
@@ -139,21 +144,55 @@
          save_fun() {
             var that = this
             var dispatch = this.$store.dispatch
+            // 保存数据
             dispatch({
                type: "baby_set_data_ac",
                data: that.data
             })
-            that.edit_mk_data_fun(false) 
+            // 隐藏模块编辑弹窗
+            that.edit_mk_data_fun(false)
+
          },
          // 宝贝数量切换
          sort_fun() {
             var that = this
             var dispatch = this.$store.dispatch
+            // 宝贝数量切换判断是否显示手动输入数量
             if (that.baby_type != "0") {
                that.data.baby_number = that.baby_type
-
             }
-         }
+         },
+         // 获取数据
+         get_data() {
+            var that = this
+            var site = that.edit_mk_data
+            // 创建空对象
+            var data_list = {};
+            // 根据位置获取模块设置数据
+            if (site.location1 == "hd") {
+               data_list = that.layout_data.hd[site.location4].data
+            }
+            if (site.location1 == "ft") {
+               data_list = that.layout_data.ft[site.location4].data
+            }
+            if (site.location1 == "con") {
+               if (site.location3 == "w19") {
+                  data_list = that.layout_data.con[site.location2].w19[site.location4].data
+               }
+               if (site.location3 == "w75") {
+                  data_list = that.layout_data.con[site.location2].w75[site.location4].data
+               }
+               if (site.location3 == "center") {
+                  data_list = that.layout_data.con[site.location2].w1920[site.location4].data
+               }
+            }
+            // 当模块内部数据有意义则更新当前数据
+            if (data_list.baby_type) {
+               that.data = data_list
+            }
+            // 还原空对象
+            data_list = {};
+         },
       }
    }
 </script>
@@ -289,6 +328,26 @@
       height: 23px;
       padding: 0 5px;
       border: 1px solid #a9a9a9
+   }
+
+   #mk1_type .con .display_set {
+      margin-bottom: 10px;
+      font-size: 12px;
+
+
+   }
+
+   #mk1_type .con .display_set .li {
+      line-height: 26px;
+   }
+
+   #mk1_type .con .display_set .li span {
+      display: inline-block;
+   }
+
+   #mk1_type .con .display_set .li span.type {
+      text-align: right;
+      width: 100px;
    }
 
    #mk1_type .con .operation {
