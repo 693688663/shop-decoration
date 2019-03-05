ca3e437607b044a21735048d826016a1654a9241
yxy982694 <37136741+yxy982694@users.noreply.github.com>
2019-03-05 10:09:42 +0800

    保存数据

--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -9,20 +9,20 @@ export default {
    get_data(site, layout_data) {
       // 根据位置获取模块设置数据
       if (site.location1 == "hd") {
-         return layout_data.hd[site.location4].data
+         return JSON.parse(JSON.stringify(layout_data.hd[site.location4].data))
       }
       if (site.location1 == "ft") {
-         return layout_data.ft[site.location4].data
+         return JSON.parse(JSON.stringify(layout_data.ft[site.location4].data))
       }
       if (site.location1 == "con") {
          if (site.location3 == "w19") {
-            return layout_data.con[site.location2].w19[site.location4].data
+            return JSON.parse(JSON.stringify(layout_data.con[site.location2].w19[site.location4].data))
          }
          if (site.location3 == "w75") {
-            return layout_data.con[site.location2].w75[site.location4].data
+            return JSON.parse(JSON.stringify(layout_data.con[site.location2].w75[site.location4].data))
          }
          if (site.location3 == "center") {
-            return layout_data.con[site.location2].w1920[site.location4].data
+            return JSON.parse(JSON.stringify(layout_data.con[site.location2].w1920[site.location4].data))
          }
       }
    },
