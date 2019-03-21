9f31b352b6e3ff6a4ad927bb11f151691fc877bc
yxy982694 <37136741+yxy982694@users.noreply.github.com>
2019-03-21 14:59:04 +0800

    element-ui 引入

--- src/main.js
+++ src/main.js
@@ -7,15 +7,18 @@ import store from './vuex/store'//状态仓库
 import VueWechatTitle from 'vue-wechat-title'//tltle
 import './components/toast/toast.css';
 import toast from './components/toast/toast';
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 Vue.use(toast);
 Vue.use(VueWechatTitle)
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
 new Vue({
-   el: '#app',
-   router,
-   store,
-   components: { App },
-   template: '<App/>'
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
 })
